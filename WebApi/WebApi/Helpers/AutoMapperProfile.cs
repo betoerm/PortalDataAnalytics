ae3cd6c467ab8ec3a5e34fba7e7a7085d05ad2a6
Beto Araújo <betoer@gmail.com>
2020-05-20 16:43:06 -0300

    Inclusão do código-fonte

---
+++ WebApi/WebApi/Helpers/AutoMapperProfile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using WebApi.Entities;
+using WebApi.Models.Users;
+
+namespace WebApi.Helpers
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<User, UserModel>();
+            CreateMap<RegisterModel, User>();
+            CreateMap<UpdateModel, User>();
+        }
+
+
+    }
+}
